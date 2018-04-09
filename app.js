8c91d6292f89405db418ec8a828415af360b4249
joakimle <joakim_le@hotmail.com>
2018-04-09 10:40:37 +0200

    updaterat skelett efter pull från master

--- app.js
+++ app.js
@@ -18,8 +18,12 @@ con.connect(function (err) {
     }
     console.log("Connected!");
 })
-app.set('port', 4000);
+app.set('port', 3000);
 
 var server = app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + server.address().port);
 });
+
+app.get('/', function(req,res) {
+    res.send('hello world');
+})
