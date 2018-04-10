8045b88195ac4779046809244a99bdd87cc0efe1
joakimle <joakim_le@hotmail.com>
2018-04-10 09:59:29 +0200

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let mysql = require('mysql');
 let express = require('express');
 let app = express();
 
-app.use(express.static('public'));
+//app.use(express.static('public'));
 
 var con = mysql.createConnection({
     host: "83.255.197.121",
@@ -12,14 +12,18 @@ var con = mysql.createConnection({
     database: "simmhoppdb"
 });
 
-con.connect(function (err) {
-    if (err) {
-        console.log('error while connectiong to database' + err);
-    }
-    console.log("Connected!");
-})
 app.set('port', 3000);
 
 var server = app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + server.address().port);
 });
+
+app.get('/', function(req,res) {
+    res.send('hello world');
+    con.connect(function (err) {
+        if (err) {
+            console.log('error while connectiong to database' + err);
+        }
+        console.log("Connected!");
+    })
+})
