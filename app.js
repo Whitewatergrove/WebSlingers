70215c74e8b008e8aafae0986aad16b16f22dd36
joakimle <joakim_le@hotmail.com>
2018-04-21 18:57:13 +0200

    callback funktion

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 'use strict'
 
-let mysql = require('mysql');
 let express = require('express');
 let app = express();
 let bodyParser = require('body-parser')
@@ -11,54 +10,54 @@ app.use(bodyParser.urlencoded({
 })); 
 app.use(express.json());     
 app.use(express.urlencoded());
-
 app.use(express.static('public'));
 
-var con = mysql.createConnection({
-    host: "83.255.197.121",
-    user: "joakim",
-    password: "jockele",
-    port: "3306",
-    database: "webslingers"
-});
-app.set('port', 3000);
+const mymodule = require('./DBfunctions')
+
+app.get('/test', function(req, res) {
+    mymodule.get_users(null, null, function(err, result){
+        if (err) throw err;
+        res.json(result);
+    })
+})
+app.set('port', 80);
 var server = app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + server.address().port);
 });
-app.get('/', function(req,res) {
-    con.connect(function (err) {
-        if (err) {
-            console.log('error while connectiong to database' + err);
-        }
-    console.log("Connected!");
-    // con.query('INSERT INTO users (ID, Password, Role) VALUES (?, ?, ?)', ['jocke2', 'Jocketest1', 'student1'], function(err, result) {
-    //     if (err) throw err
-        con.query('SELECT * FROM users', function(err, results) {
-            if (err) throw err
-            let temp = '';
-            for(var i in results) {
-                temp += ('<tr><td>' + results[i].ID + '</td><td>' + results[i].Password + '</td><td>' + results[i].Role + '</td></tr>');
-            }
-            res.write('<table>' + temp + '</table>')
-            res.end();
-            })
-        })
-        con.end;
-    // })
-})
-app.post('/test', function (req, res) {
-     var ID = req.body.username;
-     var Password = req.body.password;
-     var Role = 'student1';
-     console.log('ID = %s', ID);
-     console.log('req.body.username = %s', req.body.username);
-     console.log('Password = %s' , Password);
-     console.log('req.body.password = %s', req.body.password);
-     console.log('Role : %s',Role);
+// app.get('/', function(req,res) {
+//     con.connect(function (err) {
+//         if (err) {
+//             console.log('error while connectiong to database' + err);
+//         }
+//     console.log("Connected!");
+//     // con.query('INSERT INTO users (ID, Password, Role) VALUES (?, ?, ?)', ['jocke2', 'Jocketest1', 'student1'], function(err, result) {
+//     //     if (err) throw err
+//         con.query('SELECT * FROM users', function(err, results) {
+//             if (err) throw err
+//             let temp = '';
+//             for(var i in results) {
+//                 temp += ('<tr><td>' + results[i].ID + '</td><td>' + results[i].Password + '</td><td>' + results[i].Role + '</td></tr>');
+//             }
+//             res.write('<table>' + temp + '</table>')
+//             res.end();
+//             })
+//         })
+//         con.end;
+//     // })
+// })
+// app.post('/test', function (req, res) {
+//      var ID = req.body.username;
+//      var Password = req.body.password;
+//      var Role = 'student1';
+//      console.log('ID = %s', ID);
+//      console.log('req.body.username = %s', req.body.username);
+//      console.log('Password = %s' , Password);
+//      console.log('req.body.password = %s', req.body.password);
+//      console.log('Role : %s',Role);
      
-     con.query('INSERT INTO users (ID, Password, Role) VALUES (?, ?, ?)', [ID, Password, Role], function(err, resp) {
-             if (err) throw err
-       res.sendFile(__dirname + '/public/modalLogin.html');
-       console.log('new user added')
-    });
-});
+//      con.query('INSERT INTO users (ID, Password, Role) VALUES (?, ?, ?)', [ID, Password, Role], function(err, resp) {
+//              if (err) throw err
+//        res.sendFile(__dirname + '/public/modalLogin.html');
+//        console.log('new user added')
+//     });
+// });
