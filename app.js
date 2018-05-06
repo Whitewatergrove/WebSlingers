b00e2c5f29a0ad4cf283a6f91fb5dd8e29b57a1c
Joakim Le <joakim_le@hotmail.com>
2018-05-06 19:31:59 +0200

    testtesttest

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ const routes = require('./routes/routes');
 let session = require('express-session');
 
 app.use(session({
-    secret: 'jocketest',
+    secret: 'ufcQC`m~^8TQ-FdCRv2*YdquF3E]T`48hBA2`k%dF#dcn*&d?jbFWE*>u5zUg+cjXB.+"R$dgV]t55wS,eh+_',
     resave: false,
     saveUninitialized: false
 }))
 app.use('/', routes);
 
-app.set('port', 80);
+app.set('port', 3000);
 var server = app.listen(app.get('port'), function () {
   console.log('Express server listening on port ' + server.address().port);
 });
