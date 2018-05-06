e19968436c5b603462cd7dfb0cd34c6a392c8dcb
Joakim Le <joakim_le@hotmail.com>
2018-05-06 19:59:06 +0200

    testetsetss

--- app.js
+++ app.js
@@ -5,13 +5,28 @@ let app = express();
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 const routes = require('./routes/routes');
+
+let bcrypt = require('bcrypt');
+let cookieParser = require('cookie-parser');
 let session = require('express-session');
+let flash = require('connect-flash');
+
+
+app.use(cookieParser());
+app.use(flash());
 
 app.use(session({
     secret: 'ufcQC`m~^8TQ-FdCRv2*YdquF3E]T`48hBA2`k%dF#dcn*&d?jbFWE*>u5zUg+cjXB.+"R$dgV]t55wS,eh+_',
     resave: false,
     saveUninitialized: false
 }))
+
+
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+});
 app.use('/', routes);
 
 app.set('port', 3000);
